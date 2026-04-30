8321d68af021793f3718cbe21b0c13891edcf02d
Morgan Winn <morgank1@byu.edu>
2026-04-30 16:20:23 -0600

    Implement getPiece method

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -31,7 +31,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return squares[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
