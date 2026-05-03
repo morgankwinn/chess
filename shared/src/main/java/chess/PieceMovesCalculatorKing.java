0f5aaea080e6cf2b25f46b178cbf8048c9fda640
Morgan Winn <morgank1@byu.edu>
2026-05-02 23:11:59 -0600

    Implement kingMoveCalc in all directions

--- shared/src/main/java/chess/PieceMovesCalculatorKing.java
+++ shared/src/main/java/chess/PieceMovesCalculatorKing.java
@@ -6,8 +6,21 @@ public class PieceMovesCalculatorKing {
 
     public static void kingMoveCalc(ChessBoard board, ChessPosition position, ArrayList<ChessMove> moves) {
         // Up and to the left direction
-        moveOneDirection(board, position, moves, +1, -1);
-        // get other directions...
+        moveOneDirection(board, position, moves, 1, -1);
+        // Up direction
+        moveOneDirection(board, position, moves, 1, 0);
+        // Up and to the right direction
+        moveOneDirection(board, position, moves, 1, 1);
+        // Right direction
+        moveOneDirection(board, position, moves, 0, 1);
+        // Down and to the right direction
+        moveOneDirection(board, position, moves, -1, 1);
+        // Down direction
+        moveOneDirection(board, position, moves, -1, 0);
+        // Down and to the left direction
+        moveOneDirection(board, position, moves, -1, -1);
+        // Left direction
+        moveOneDirection(board, position, moves, 0, -1);
     }
 
     private static void moveOneDirection(ChessBoard board, ChessPosition position, ArrayList<ChessMove> moves, int newRowAdd, int newColAdd) {
@@ -16,12 +29,15 @@ public class PieceMovesCalculatorKing {
 
         int newRow = position.getRow()+newRowAdd;
         int newCol = position.getColumn()+newColAdd;
-        ChessPosition newPos = new ChessPosition(newRow, newCol);
 
-        if (board.getPiece(newPos) != null) {
-            ChessPiece new_piece = board.getPiece(newPos);
-            if (new_piece.getTeamColor() == team) { return; }
+        if (newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8) {
+            ChessPosition newPos = new ChessPosition(newRow, newCol);
+
+            if (board.getPiece(newPos) != null) {
+                ChessPiece new_piece = board.getPiece(newPos);
+                if (new_piece.getTeamColor() == team) { return; }
+            }
+            moves.add(new ChessMove(position, newPos, null));
         }
-        moves.add(new ChessMove(position, newPos, null));
     }
 }
