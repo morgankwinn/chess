1f83f0de46e45b6f4d10501ba6525013dcf35d60
Morgan Winn <morgank1@byu.edu>
2026-05-18 15:54:27 -0600

    Implement server exception handler

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,11 +1,14 @@
 package server;
 
+import com.google.gson.Gson;
 import dataaccess.AuthDAO;
 import dataaccess.MemoryAuthDAO;
 import dataaccess.MemoryUserDAO;
 import dataaccess.UserDAO;
 import handler.RegisterHandler;
 import io.javalin.*;
+import io.javalin.http.Context;
+import service.AlreadyTakenException;
 
 public class Server {
 
@@ -15,7 +18,8 @@ public class Server {
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"))
-                .post("/user", context -> RegisterHandler.handleRegister(context, userDao, authDao));
+                .post("/user", context -> RegisterHandler.handleRegister(context, userDao, authDao))
+                .exception(AlreadyTakenException.class, this::handleException);
 
         // Register your endpoints and exception handlers here.
     }
@@ -27,5 +31,9 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private String handleException(AlreadyTakenException e, Context context) {
+        return new Gson().toJson(new Exception(e + ": " + context.body()));
     }
 }
