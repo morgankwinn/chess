f910088c9f9d2fd90dc0dc1d4f8f8cf1171352ba
Morgan Winn <morgank1@byu.edu>
2026-06-09 17:18:58 -0600

    Add ErrorMessage and NotificationMessage

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,16 @@
+package websocket.messages;
+
+import model.Game;
+
+public class ErrorMessage extends ServerMessage {
+    private final String errorMessage;
+
+    public ErrorMessage(ServerMessageType type, String errorMessage) {
+        super(type);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
